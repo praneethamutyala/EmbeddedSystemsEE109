a31938f45ececb1ec5252c58e757da00578c63b0
Sai Praneetha Mutyala <smutyala@usc.edu>
2018-03-18 15:49:50 -0700

    Add files via upload

---
+++ lab0.c
@@ -0,0 +1,26 @@
+/********************************************
+*
+*  Name: Sai Praneetha Mutyala	
+*  Section: 31291
+*  Assignment: Lab 0 - Blink the LED on Port B, bit 5
+*
+********************************************/
+
+#include <avr/io.h>
+#include <util/delay.h>
+
+int main(void)
+{
+    // Your program goes here
+	DDRB |= (1 << DDB5); /* Set PB5 for output */
+	
+
+    while(1) {
+		PORTB |= (1 << PB5);     /* LED on */
+		_delay_ms(500);          /* Wait 500 milliseconds */
+		PORTB &= ~(1 << PB5);    /* LED off */
+		_delay_ms(500);          /* Wait 500 milliseconds */
+    }
+
+    return 0;   /* never reached */
+}
